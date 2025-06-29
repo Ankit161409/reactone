7273ffa12adea1ab461ab604e9b474254e2ea3a8
Pooja Bharti <53570733+puja2318@users.noreply.github.com>
2025-06-29 23:33:04 +0530

    new project

---
+++ src/Ak.js
@@ -0,0 +1,18 @@
+import './ak.css';
+
+export default function Aboutpage()
+{
+   return  <h1>this is from default aboutpage function</h1>
+}
+export function Aboutpage1()
+{
+   return( 
+   <div> <h1 class="h1">this is from aboutpage1 name  function</h1></div>
+);
+}
+export function Aboutpage2()
+{
+   return   <h1>this is from aboutpage2 name  function</h1>
+}
+  
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Xyz ,{Aboutpage1,Aboutpage2} from './Ak';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <Xyz></Xyz>
+   <Aboutpage2/>
+   <Aboutpage1/>
+   
+   
+  </React.StrictMode>
+);
+
+ /*
+ However, to use it as a JSX element, the first letter must be uppercase.
+Why Capital Letter
+=>React treats lowercase JSX tags as HTML elements (<div>, <span>, etc.)
+=>Uppercase names are interpreted as custom React components.
+ <ABC />  react treat as a custom react component .This works because ABC starts with uppercase 
+<abc /> // ❌ React treats this as a custom HTML tag, not a component
+*/
