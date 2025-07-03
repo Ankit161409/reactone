78e6a8305aa0e25fd9f76b802674f2d781c90f88
Pooja Bharti <53570733+puja2318@users.noreply.github.com>
2025-07-03 14:26:12 +0530

    changes in css

--- src/Ak.js
+++ src/Ak.js
@@ -19,7 +19,7 @@ export function Aboutpage2()
    let sub ="java";
    let working  ="true";
    return(
-      <Fragment>
+      <Fragment>        
        <div className='div'> 
       <h1 className='h12'>this is from aboutpage2 ankit function</h1></div>
       <h2 className='h12'>this is from aboutpage2 name  function</h2>
@@ -28,10 +28,14 @@ export function Aboutpage2()
       <h1>{sub}</h1>
       <h1>{working}</h1>
       <h1>{<Aboutpage1/> }</h1>
-      {/* <h1>{ name=="ankit" ? "yes" : "no" }</h1> */}
+      <h1>{ name==="ankit" ? "yes" : "no" }</h1>
       <h1>{<span>this is my element</span>}</h1>
+      <h1 style={{color:'red',backgroundColor:'green'}}>this is inline css </h1>
 </Fragment>
    )
 }
+
+
+//If you don’t want to wrap your elements in a div or another HTML tag then use fragment
   
 
